0be7b956a350f44a6b5992e469b4278c46336ada
henry <hmarichal93@gmail.com>
2024-07-29 12:51:09 -0300

    adding postprocessing step

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -14,8 +14,9 @@ model=$8
 # Extract center from mask
 python $BIN/.ipol/preprocessing.py --input_poly $input_1 --input_img $input_0 --mask_path $output_0 --hsize $hsize --wsize $wsize
 
+# Run INBD inference
 python $BIN/INBD/main.py inference $BIN/models/$model $input_0 $output_0 --output $HOME/output/
 
 #process the output to get the final result
-python $BIN/src/
+python $BIN/src/from_inbd_to_urudendro_labels.py --root_dataset ROOT_DATASET --root_inbd_results INFERENCE_OUTPUT --output_dir OUTPUT_DIR
 
