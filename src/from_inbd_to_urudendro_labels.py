1582da4cec0d7bbf8a7d11e43234adf7f669ace7
henry <hmarichal93@gmail.com>
2024-08-08 12:07:58 -0300

    debug fix

--- src/from_inbd_to_urudendro_labels.py
+++ src/from_inbd_to_urudendro_labels.py
@@ -176,7 +176,7 @@ def main(root_dataset = "/data/maestria/datasets/Candice_inbd_1500/",
         conversor.generate_pdf()
 
     print(f"Labels are stored in {output_dir}")
-
+    raise
     return output_dir
 
 if __name__ == "__main__":
