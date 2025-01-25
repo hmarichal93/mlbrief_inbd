9d7ae45da8955d6567528c5d3f70fe1967a69b70
henry <hmarichal93@gmail.com>
2025-01-25 09:22:11 -0300

    logs

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=inbd
-#SBATCH --ntasks=16
-#SBATCH --mem=100G
+#SBATCH --job-name=deep_cstrd
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=30G
 #SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --tmp=100G
