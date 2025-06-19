e2580c4b4f707952a423e0dc9ff83c0d916b218d
henry <hmarichal93@gmail.com>
2025-06-19 12:26:15 -0300

    run script

--- cluster.sh
+++ cluster.sh
@@ -1,88 +1,67 @@
 #!/bin/bash
-#SBATCH --job-name=deep_cstrd
+#SBATCH --job-name=inbd
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=30G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --tmp=100G
 #SBATCH --mail-user=henry.marichal@fing.edu.uy
-
-# de acuerdo a lo que quiera ejecutar puede elegir entre las siguientes tres líneas.
-#SBATCH --gres=gpu:1 # se solicita una gpu cualquiera( va a tomar la primera que quede disponible indistintamente si es una p100 o una a100)
-
-
+#SBATCH --gres=gpu:a40:1
 #SBATCH --partition=normal
 #SBATCH --qos=gpu
 
 
+
+# Cargar módulos y activar el entorno
 source /etc/profile.d/modules.sh
 source /clusteruy/home/henry.marichal/miniconda3/etc/profile.d/conda.sh
-conda activate inbd_gpu
+conda activate deep_cstrd
 
-# -------------------------------------------------------
-#disco local SSD local al nodo. /clusteruy/home/henry.marichal se accede via NFS (puede ser realmente lento)
-#el espacio local a utilizar se reserva dcon --tmp=XXXGb
-ROOT_DIR=$2
-HOME_DATASET_DIR=$3
-HOME_RESULTADOS_DIR=$4
-SEGMENTATION_MODEL=$5
-IMAGES_FILE=$6
-ANNOTATIONS_FILE=$7
-LOCAL_NODE_DIR=/scratch/henry.marichal/
-HOME_RESULTADOS_MODEL_DIR=$HOME_RESULTADOS_DIR/model
+# Variables
+ROOT_DIR=$1
+HOME_DATASET_DIR=$2
+HOME_RESULTADOS_DIR=$3
+DATASET_NAME=$4
+NODE_DATASET_DIR=/scratch/henry.marichal/
+NODE_RESULTADOS_DIR=$NODE_DATASET_DIR/results
+EPOCHS=$5
 
-# -------------------------------------------------------
-#other variables
-#NODE_RESULTADOS_DIR=$LOCAL_NODE_DIR/inbd/resultados
-#NODE_DATASET_DIR=$LOCAL_NODE_DIR/inbd/EH
-
-#NODE_MODEL_RESULTADOS_DIR=$NODE_RESULTADOS_DIR/model
-stdout_file="$HOME_RESULTADOS_DIR/stdout.txt"
-stderr_file="$HOME_RESULTADOS_DIR/stderr.txt"
-# Define a function to check the result of a command
+# Función para verificar el resultado de un comando
 check_command_result() {
-    # Run the command passed as an argument
     "$@"
-
-    # Check the exit status
-    if [ $? -eq 0 ]; then
-        echo "Command was successful."
-    else
-        echo "Command failed with an error."
+    if [ $? -ne 0 ]; then
+        echo "Error: El comando falló."
         exit 1
     fi
 }
 
-####Prepare directories
-#rm -rf $NODE_DATASET_DIR
-#rm -rf $NODE_RESULTADOS_DIR
-#rm -rf $HOME_RESULTADOS_DIR
+# Preparar directorios
+check_command_result rm -rf $NODE_DATASET_DIR $NODE_RESULTADOS_DIR
+check_command_result mkdir -p $NODE_DATASET_DIR $NODE_RESULTADOS_DIR
 
-#check_command_result mkdir -p $NODE_DATASET_DIR
-#check_command_result mkdir -p $NODE_RESULTADOS_DIR
-check_command_result mkdir -p $HOME_RESULTADOS_DIR
+# Copiar dataset al disco local del nodo
+check_command_result cp -r $HOME_DATASET_DIR $NODE_DATASET_DIR
 
-####Move dataset to node local disk
-#check_command_result cp  -r $HOME_DATASET_DIR $NODE_DATASET_DIR
-
-
-# -------------------------------------------------------
-# Run the program
+# Entrenar el modelo
 cd $ROOT_DIR
 
-if [ "$1" == "segmentation" ]; then
-    echo "Segmentation"
-    python main.py train segmentation $HOME_DATASET_DIR/$IMAGES_FILE $HOME_DATASET_DIR/$ANNOTATIONS_FILE \
-    --output $HOME_RESULTADOS_MODEL_DIR --epochs 100 --downsample 1 --transfer_learning --model_path $SEGMENTATION_MODEL > "$stdout_file" 2> "$stderr_file"
-fi
-if [ "$1" == "INBD" ]; then
-    echo "INBD"
-    INBD_MODEL=$8
-    python main.py train INBD $HOME_DATASET_DIR/$IMAGES_FILE $HOME_DATASET_DIR/$ANNOTATIONS_FILE \
-           --segmentationmodel=$SEGMENTATION_MODEL --downsample 1 --output $HOME_RESULTADOS_MODEL_DIR --epochs 100\
-           --transfer_learning --model_path $INBD_MODEL > "$stdout_file" 2> "$stderr_file"
-fi
+for i in {1..5}; do
+  MODEL_DIR="$NODE_RESULTADOS_DIR/model_$i"
+  check_command_result mkdir -p $MODEL_DIR
+  python main.py train segmentation $NODE_DATASET_DIR/$DATASET_NAME/$IMAGES_FILE $NODE_DATASET_DIR/$DATASET_NAME/$ANNOTATIONS_FILE \
+      --output $MODEL_DIR --epochs $EPOCHS --downsample 1
+
+  SEGMENTATION_MODEL=$(find ${MODEL_DIR} -name model.pt.zip)
+
+  python main.py train INBD $NODE_DATASET_DIR/$DATASET_NAME/$IMAGES_FILE $NODE_DATASET_DIR/$DATASET_NAME/$ANNOTATIONS_FILE \
+         --segmentationmodel=$SEGMENTATION_MODEL --downsample 1 --output $MODEL_DIR --epochs $EPOCHS
+
+  HOME_ITERATION_DIR="$HOME_RESULTADOS_DIR/model_$i"
+  check_command_result mkdir -p $HOME_ITERATION_DIR
+  check_command_result cp -r $MODEL_DIR $HOME_ITERATION_DIR
+
+done
 
 
 # -------------------------------------------------------
